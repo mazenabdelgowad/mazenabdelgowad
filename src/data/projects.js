65d0f37a53d8ee4ad223c42a66785ab3992fcde4
Mazen Abdelgawad <mazenabdelgawad700@gmail.com>
2026-09-09 04:39:20 +0300

    add new project and add expand project image feature

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,4 +1,6 @@
-import cookitEatItImage from "../assets/image.png";
+import cookitEatItImage from "../assets/cookitEatItImage.png";
+import MedScanAI_Image from "../assets/MedScanAI_Image.jpg";
+
 
 const projects = [
   {
@@ -16,7 +18,23 @@ const projects = [
     githubUrl: "https://github.com/mazenabdelgawad700/CookItEatIt",
     technologies: [".NET Core", "SQL Server", "Flutter"],
     featured: true,
-  }
+  },
+  {
+    id: "MedScanAI",
+    title: "MedScanAI - Capstone Project",
+    description:
+      "MediScan is a full-stack medical diagnostic platform that uses deep learning to help clinicians and patients analyze medical images across four critical domains: skin diseases, brain tumors, breast cancer, and chest X-ray abnormalities. ",
+    category: "software",
+    type: "Web Application",
+    media: {
+      src: MedScanAI_Image,
+      alt: "MedScanAI - Smart medical diagnostic platform UI",
+      type: "image",
+    },
+    githubUrl: "https://github.com/ahmed-talha-ai/MedScanAI-Graduation-Project",
+    technologies: [".NET Core", "SQL Server", "React js"],
+    featured: true,
+  },
 ];
 
 export default projects;
